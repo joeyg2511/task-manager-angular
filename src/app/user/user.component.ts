f72f9efd9332d5233649e2937e0bda81afb784a1
jga2488 <jga2488@wgu.edu>
2025-08-03 13:56:14 -0400

    Created a list of users. I used the Input decorator to pass data from the app.component(parent) to the user.component(child).

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,19 +9,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  @Input ({required: true}) avatar! : string;
+  @Input ({required: true}) name! : string;
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
-
-  /*get imagePath() {
-    return '/assets/users/' + this.selectedUser.avatar; <- This also uses the Zone.js method to change the content
+  get imagePath() {
+    return "assets/users/" + this.avatar;
   }
-    */ 
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]); 
-    //this.selectedUser = DUMMY_USERS[randomIndex]; <- This uses the Zone.js and the state method to change the content
 
   }
 
